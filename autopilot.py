d3a1242f5f5f1cbf1b088df89178cf77963f0378
Rémy Hidra <remy.hidra@gmail.com>
2020-07-27 17:27:06 +0200

    Rearrange code

--- autopilot.py
+++ autopilot.py
@@ -2,17 +2,21 @@
 from mapping import getWorld, localToGlobal, ORIGIN_LAT, ORIGIN_LON
 from planning import dummyPath
 from node import MissionNode
+import rospy
 
 
 def start(planning_algo):
+    node = MissionNode(node_name='autopilot')
+    node.setup()
+
+    # rospy.loginfo(node.octomap)
+
     # Path planning
     path = planning_algo(None)
 
     # Convert the path to global GPS coordinates
     path_global = localToGlobal(ORIGIN_LAT, ORIGIN_LON, path)
 
-
     # Send the path as a mission to MAVROS
-    node = MissionNode(node_name='autopilot')
-    node.load_waypoints(path_global)
-    node.main()
+    # node.load_waypoints(path_global)
+    # node.exec_mission()
