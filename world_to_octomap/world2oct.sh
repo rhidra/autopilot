64b7e80172226f9b77ef21d76a6afc47ed8ca267
Rémy Hidra <remy.hidra@gmail.com>
2020-08-11 15:26:52 +0200

    Fix shell script

--- world_to_octomap/world2oct.sh
+++ world_to_octomap/world2oct.sh
@@ -30,7 +30,7 @@ rm /tmp/file.dae /tmp/file.obj /tmp/file.binvox /tmp/file.binvox.bt
 python3 ${__dir}/world2dae.py $in /tmp/file.dae $axis_up
 
 # Convert from Collada .dae to .obj using Blender
-blender --background --python ${__dir}/blend_convert_to_obj.py -- /tmp/file.dae /tmp/file.obj
+blender --background --python ${__dir}/blend_convert_to_obj.py -- /tmp/file.dae /tmp/file.obj $axis_up
 
 # Convert from .obj to voxels with binvox
 # Assuming the binvox executable is in $HOME
