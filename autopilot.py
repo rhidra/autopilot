3f343b95d5e6902d3f7a5e874965795c8a79f684
Rémy Hidra <remy.hidra@gmail.com>
2020-08-11 17:49:31 +0200

    Minor changes

--- autopilot.py
+++ autopilot.py
@@ -8,13 +8,13 @@ def start(planning_algo):
     node.setup()
 
     # Path planning
-    path, _, _, _ = planning_algo(node, [0, 0, 4], [0, -10.5, 4], world_dim=[-20, 20, -10, 10, 0, 2.8*2])
+    path, _, _, _ = planning_algo(node, [0, 0, 4], [0, -10.5, 4], world_dim=[-20, 20, -10, 10, 0, 5.5])
+
     # path, _, _, _ = planning_algo(node, [0, 0, 4], [0, 10, 4], world_dim=[-20, 20, -10, 10, 0, 2.8*2])
     # path, _, _, _ = planning_algo(node, [0, -10.5, 4], [0, 0, 4], world_dim=[-20, 20, -10, 10, 0, 2.8*2])
 
     # path, _, _, _ = planning_algo(node, [0, 0, 4], [-3.5, -3.2, 7], world_dim=[-100, 100, -100, 100, 0, 50])
     # path, _, _, _ = planning_algo(node, [-3.5, -3.2, 7], [0, 0, 4], world_dim=[-100, 100, -100, 100, 0, 50])
-    rospy.loginfo(path)
 
     node.load_local_path(path)
 
