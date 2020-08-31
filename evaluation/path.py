c63388e37d34ee016701dadbd60432f0dd2e68e8
Rémy Hidra <remy.hidra@gmail.com>
2020-08-31 18:54:22 +0200

    Minor fix

--- evaluation/path.py
+++ evaluation/path.py
@@ -60,7 +60,7 @@ def path_rotation(path):
 
 
 def angle_between(v1, v2):
-    # Returns the angle in radians between vectors 'v1' and 'v2'
+    # Returns the angle between vectors 'v1' and 'v2'
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)) * 180 / np.pi
