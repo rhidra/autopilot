8ad1c4f0c1f6ced7d980d6bec464ff26bec7d63c
Rémy Hidra <remy.hidra@gmail.com>
2021-03-29 17:25:54 +0200

    Add the outline for an uncertain final position and velocity traj request

--- node/motion_primitive_node.py
+++ node/motion_primitive_node.py
@@ -10,6 +10,7 @@ from octomap_node import OctomapNode
 from motion_primitive import MotionPrimitiveLibrary, TrajectoryError
 from autopilot.msg import MotionPrimitive
 from autopilot.srv import LocalGoal, LocalGoalRequest
+from controller_msgs.msg import FlatTarget
 
 TOLERANCE_FROM_GOAL = 0.5
 class MotionPrimitiveNode(OctomapNode):
@@ -20,14 +21,16 @@ class MotionPrimitiveNode(OctomapNode):
         self.trajectory = None
         self.current_traj = -1
         self.traj_history = []
-        self.trajectory_sub = rospy.Subscriber('/autopilot/trajectory/request', Empty, self.send_trajectory)
+        self.trajectory_sub = rospy.Subscriber('/autopilot/trajectory/request', FlatTarget, self.send_trajectory)
         self.trajectory_pub = rospy.Publisher('/autopilot/trajectory/response', MotionPrimitive, queue_size=10)
         self.get_local_goal_srv = rospy.ServiceProxy('/autopilot/local_goal', LocalGoal)
         rospy.loginfo('Waiting for trajectory request...')
         rospy.spin()
     
-    def send_trajectory(self, _):
-        if not self.generate_trajectory():
+    def send_trajectory(self, msg):
+        pos0 = np.array([msg.position.x, msg.position.y, msg.position.z])
+        vel0 = np.array([msg.velocity.x, msg.velocity.y, msg.velocity.z])
+        if not self.generate_trajectory(pos0, vel0):
             return
         self.current_traj += 1
         rospy.loginfo('Sending traj {}'.format(self.current_traj))
@@ -35,15 +38,13 @@ class MotionPrimitiveNode(OctomapNode):
         self.trajectory_pub.publish(msg)
         self.visualize_local_path(trajLibrary=self.mpl.trajs, trajSelected=self.traj_history[self.current_traj], trajHistory=self.traj_history, tf=self.tf)
 
-    def generate_trajectory(self):
+    def generate_trajectory(self, pos0, vel0):
         if self.local_goal_point is None or self.local_goal_direction is None:
             rospy.logerr('Cannot generate a trajectory: No local goal received !')
-
+            
         rospy.loginfo('*' * 30)
-        prev_traj = None if self.current_traj == -1 else self.traj_history[self.current_traj]
-
         try:
-            traj = self.compute_optimal_traj(prev_traj)
+            traj = self.compute_optimal_traj(pos0, vel0)
             self.traj_history.append(traj)
             return True
         except TrajectoryError as e:
@@ -53,29 +54,22 @@ class MotionPrimitiveNode(OctomapNode):
             return False
     
 
-    def compute_optimal_traj(self, traj_prev=None):
+    def compute_optimal_traj(self, pos0=None, vel0=None):
         start = time.time()
-        if traj_prev is None:
-            traj_prev = self.trajectory
-
-        if traj_prev is None:
-            pos, vel, acc, yaw = self.pos, self.vel, self.acc, self.yaw
-            vel, acc = np.array([0, 0, 0]), np.array([0, 0, 0])
-            local_goal_point, local_goal_direction = self.get_local_goal(pos, vel)
-            yaw = np.arctan2(local_goal_point[1] - pos[1], local_goal_point[0] - pos[0])
+        if pos0 is None or vel0 is None:
+            pos0, vel0, acc0 = self.pos, self.vel, self.acc
+            local_goal_point, local_goal_direction = self.get_local_goal(pos0, vel0)
         else:
-            pos, vel = traj_prev.get_position(self.tf), traj_prev.get_velocity(self.tf), 
-            acc, yaw = traj_prev.get_acceleration(self.tf), traj_prev.get_yaw(self.tf), 
-            local_goal_point, local_goal_direction = self.get_local_goal(pos, vel)
+            acc0 = np.array([0, 0, 0])
+            local_goal_point, local_goal_direction = self.get_local_goal(pos0, vel0)
             
-        if np.linalg.norm(vel) < 0.2: 
+        if np.linalg.norm(vel0) < 0.2: 
             # When the UAV stopped, we artificially increase its 
             # initial velocity to make it move in the right direction
-            vel = np.array([.1 * np.cos(yaw), .1 * np.sin(yaw), 0.])
-            acc = np.array([0., 0., 0.])
+            vel0 = np.array([.1 * np.cos(self.yaw), .1 * np.sin(self.yaw), 0.])
 
         self.mpl = MotionPrimitiveLibrary(tf=self.tf)
-        self.mpl.generate_traj_library(pos, vel, acc, local_goal_point[2])
+        self.mpl.generate_traj_library(pos0, vel0, acc0, local_goal_point[2])
         self.mpl.rank_trajectories(local_goal_point, local_goal_direction, self.get_point_edt)
         traj = self.mpl.get_best_traj()
         rospy.loginfo('Generated a new trajectory in {}sec'.format(time.time() - start))
--- node/trajectory_sampler_node.py
+++ node/trajectory_sampler_node.py
@@ -23,7 +23,7 @@ class TrajectorySamplerNode(OctomapNode):
         super(TrajectorySamplerNode, self).setup()
         self.rate = rospy.Rate(50)
         self.trajectory_sub = rospy.Subscriber('/autopilot/trajectory/response', MotionPrimitive, self.load_trajectory)
-        self.trajectory_pub = rospy.Publisher('/autopilot/trajectory/request', Empty, queue_size=10)
+        self.trajectory_pub = rospy.Publisher('/autopilot/trajectory/request', FlatTarget, queue_size=10)
         self.traj_tracking_pub = rospy.Publisher('/reference/flatsetpoint', FlatTarget, queue_size=10)
         self.yaw_tracking_pub = rospy.Publisher('/reference/yaw', Float32, queue_size=10)
         self.trajectory = None
@@ -45,8 +45,31 @@ class TrajectorySamplerNode(OctomapNode):
         if self.has_requested:
             return
         self.has_requested = True
-        rospy.loginfo('Requesting trajectory...')
-        self.trajectory_pub.publish(Empty())
+
+        if self.trajectory is None:
+            posf, velf = self.pos, self.vel
+        else:    
+            # Estimate the final position and velocity from the current real world data
+            # We could use the trajectory final pose, but it would not consider real world uncertainties
+            # Instead, we consider the current acceleration, velocity and position
+            # and we model the acceleration as an affine function with a(tf)=[0,0,0].
+            # From this assumption, we derive the final estimated velocity and position.
+            # t0: start traj; t1=0: now in the traj; tf: end of the traj
+            # We know a(t1)=a(0); v(t1)=v(0) and p(t1)=0
+            # a(t) = a(t1) - a(t1) / (tf - t1) * (t - t1) with t1=0
+            # So we have: 
+            #   a(t) = a(0) - a(0) * t / tf
+            #   v(t) = v(0) + a(0)*t - a(0) * t*t/(2*tf) => v(tf) = v(0) + a(0)*tf/2
+            #   p(t) = p(0) + v(0)*t + a(t)*t*t/2 - a(0)*t*t*t/(6*tf) => p(tf) = p(0) + v(0)*tf + a(0)*tf*tf/3
+            # Here tf is actually the duration to reach the end of the traj
+
+            # tf = self.trajectory._tf - (rospy.Time.now() - self.traj_start).to_sec()
+            # velf = self.vel + self.acc * tf / 2.
+            # posf = self.pos + self.vel * tf + self.acc * tf * tf / 3.
+            posf = self.trajectory.get_position(self.trajectory._tf)
+            velf = self.trajectory.get_velocity(self.trajectory._tf)
+        rospy.loginfo('Requesting trajectory with pos0={} and vel0={}...'.format(posf, velf))
+        self.trajectory_pub.publish(build_traj_tracker(pos=posf, vel=velf))
 
 
     def execute_trajectory(self):
@@ -76,11 +99,12 @@ class TrajectorySamplerNode(OctomapNode):
             if self.trajectory is None and self.next_trajectory is None:
                 self.rate.sleep()
                 continue
-            
+
             t = (rospy.Time.now() - self.traj_start).to_sec()
 
             try:
-                if self.next_trajectory is None:
+                # Only try to generate a new trajectory before a few milliseconds before reaching the primitive end
+                if self.next_trajectory is None and t >= self.trajectory._tf - .7:
                     self.request_trajectory()
 
                 # If no trajectory or if we reached the end, we change trajectory
