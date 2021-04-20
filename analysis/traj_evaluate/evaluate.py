a6bed8b37c2da00894cf300fd10c91b3fb532ced
Rémy Hidra <remy.hidra@gmail.com>
2021-04-20 21:39:09 +0200

    Automate the entire evaluation pipeline

---
+++ analysis/traj_evaluate/evaluate.py
@@ -0,0 +1,60 @@
+import csv, subprocess, time, os, signal
+
+# Number of trials to do on the same map and start/goal couple
+N_TRIAL = 10
+
+def trialGenerator():
+    with open('/home/rhidra/forest_gen/start_and_end.csv', 'r') as f:
+        try:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                configId = int(row[0]) % 100
+                mapId = int(row[1])
+                start = [float(row[2]), float(row[3]), float(row[4])]
+                goal = [float(row[5]), float(row[6]), float(row[7])]
+
+                if mapId >= 2:
+                    print('Reached mapId: {}'.format(mapId))
+                    print('Exiting')
+                    exit(0)
+
+                for i in range(N_TRIAL):
+                    yield i, configId, mapId, start, goal
+
+        except Exception as e:
+            print(e)
+
+def main():
+    for trialId, configId, mapId, start, goal in trialGenerator():
+        filename = 'test_m{}_c{}_t{}.json'.format(mapId, configId, trialId)
+
+        if os.path.exists('/home/rhidra/research_data/{}'.format(filename)):
+            continue
+
+        proc = subprocess.Popen(['roslaunch', 'autopilot', 'simulation.launch', 'monitoring:=true',
+                                'x:={}'.format(start[0]), 
+                                'y:={}'.format(start[1]), 
+                                'z:={}'.format(start[2]), 
+                                'goal_x:={}'.format(goal[0]), 
+                                'goal_y:={}'.format(goal[1]), 
+                                'goal_z:={}'.format(goal[2]), 
+                                'world:=forest{}'.format(mapId), 
+                                'trialId:={}'.format(trialId),
+                                'configId:={}'.format(configId),
+                                'mapId:={}'.format(mapId)])
+        pid = proc.pid
+
+        print('Pipeline launched for trialId: {} | configId: {} | mapId: {}'.format(trialId, configId, mapId))
+
+        while not os.path.exists('/home/rhidra/research_data/{}'.format(filename)):
+            time.sleep(5)
+
+        print('Pipeline completed. Killing roslaunch...')
+        os.kill(pid, signal.SIGINT)
+        time.sleep(15)
+
+        
+
+if __name__ == '__main__':
+    main()
