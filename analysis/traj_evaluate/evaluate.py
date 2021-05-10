d5e62696f339846cad138d8f68a6e6df40dae6c3
Rémy Hidra <remy.hidra@gmail.com>
2021-05-10 11:21:16 +0200

    Add  temporary results to notebook

--- analysis/traj_evaluate/evaluate.py
+++ analysis/traj_evaluate/evaluate.py
@@ -14,11 +14,8 @@ def trialGenerator():
                 mapId = int(row[1])
                 start = [float(row[2]), float(row[3]), float(row[4])]
                 goal = [float(row[5]), float(row[6]), float(row[7])]
-
-                if mapId != 4:
-                    continue
                 
-                if mapId > N_MAPS - 1:
+                if mapId > N_MAPS:
                     print('Reached mapId: {}'.format(mapId))
                     print('Exiting')
                     exit(0)
